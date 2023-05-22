4415634c70e3f09847d3479c89d976caf04ba309
StrongWise <ghoul300@naver.com>
2023-05-22 17:55:55 +0900

    temp

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
   css: ['element-ui/lib/theme-chalk/index.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['@/plugins/element-ui'],
+  plugins: ['@/plugins/element-ui'
+  ,{ src: '~/plugins/vee-validate.js', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
